dfb1aaf13a5fc0932e81223b93fa24625d006ca5
Jao Angeloni <joaopedroangeloni@gmail.com>
2023-07-28 07:48:27 -0300

    PROJ

---
+++ api/prisma/migrations/20230728104435_descricao/migration.sql
@@ -0,0 +1,68 @@
+-- CreateTable
+CREATE TABLE `Cardapio` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `produto` VARCHAR(191) NOT NULL,
+    `descricao` VARCHAR(191) NOT NULL,
+    `preco` DOUBLE NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Motoboy` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nome` VARCHAR(191) NOT NULL,
+    `placa` VARCHAR(191) NOT NULL,
+    `modelo` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Cliente` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `cpf` VARCHAR(191) NOT NULL,
+    `telefones` VARCHAR(191) NOT NULL,
+    `enderecoCep` VARCHAR(191) NOT NULL,
+    `enderecoNumero` VARCHAR(191) NOT NULL,
+    `enderecoComplemento` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Cliente_cpf_key`(`cpf`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Pedido` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `clienteId` INTEGER NOT NULL,
+    `motoboyId` INTEGER NOT NULL,
+    `dataPedido` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `dataCozinha` DATETIME(3) NULL,
+    `dataEntrega` DATETIME(3) NULL,
+    `valorPedido` DOUBLE NOT NULL,
+    `valorEntrega` DOUBLE NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Itens` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `pedidoId` INTEGER NOT NULL,
+    `cardapioId` INTEGER NOT NULL,
+    `quantidade` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Pedido` ADD CONSTRAINT `Pedido_clienteId_fkey` FOREIGN KEY (`clienteId`) REFERENCES `Cliente`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Pedido` ADD CONSTRAINT `Pedido_motoboyId_fkey` FOREIGN KEY (`motoboyId`) REFERENCES `Motoboy`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Itens` ADD CONSTRAINT `Itens_pedidoId_fkey` FOREIGN KEY (`pedidoId`) REFERENCES `Pedido`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Itens` ADD CONSTRAINT `Itens_cardapioId_fkey` FOREIGN KEY (`cardapioId`) REFERENCES `Cardapio`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
