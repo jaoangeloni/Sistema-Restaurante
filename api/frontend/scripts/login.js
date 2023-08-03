45763dd703b470db3b220eddc1447193f738ef3e
Jao Angeloni <joaopedroangeloni@gmail.com>
2023-08-03 07:57:03 -0300

    PROJ

--- api/frontend/scripts/login.js
+++ api/frontend/scripts/login.js
@@ -10,3 +10,15 @@ function modalLogin() {
     formLogin.classList.toggle("displayNone")
     formCadastro.classList.add("displayNone")
 }
+
+const iCpf = document.querySelector("#cpf")
+const iNome = document.querySelector("#nome")
+const iTelefone = document.querySelector("#telefone")
+const iNum = document.querySelector("#num")
+const iCep = document.querySelector("#cep")
+const iSenha = document.querySelector("#senha")
+const iConfirmarSenha = document.querySelector("#confirmarSenha")
+
+function cadastrarDados() {
+
+}
