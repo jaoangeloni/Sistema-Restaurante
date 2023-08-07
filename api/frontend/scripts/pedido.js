c774ea82be67efcaedf7e84093dbdbc6f120c9a4
Jao Angeloni <joaopedroangeloni@gmail.com>
2023-08-07 10:08:50 -0300

    PROJ

--- api/frontend/scripts/pedido.js
+++ api/frontend/scripts/pedido.js
@@ -1,15 +1,11 @@
-const formCadastro = document.querySelector(".cadastrar");
-
-function modalCadastro() {
-    formCadastro.classList.toggle("displayNone");
-};
-
 const iCpf = document.querySelector("#cpf");
 const iNome = document.querySelector("#nome");
 const iTel = document.querySelector("#telefone");
 const iNum = document.querySelector("#num");
 const iCep = document.querySelector("#cep");
 const iComplemento = document.querySelector("#complemento");
+
+const itens = document.querySelector(".itens")
 
 function cadastrarDados() {
     let cliente = {
@@ -32,4 +28,65 @@ function cadastrarDados() {
             iComplemento.value = "";
         })
 
+}
+
+
+api.get("/cardapio/")
+    .then(resp => {
+        const dados = resp.data
+        dados.forEach(item => {
+            criarCard(item)
+        });
+    })
+
+function criarCard(imovel) {
+    let card = document.createElement("div")
+    card.classList.add("card")
+
+    let nome = document.createElement("p")
+
+    let imagem = document.createElement("img")
+
+    let descricao = document.createElement("p")
+
+    descricao.classList.add("descricao")
+    let preco = document.createElement("p")
+
+    let div = document.createElement("div")
+    div.classList.add("quantidade")
+
+    let minus = document.createElement("span")
+    minus.innerHTML = "-"
+    minus.classList.add("botoes")
+
+    let plus = document.createElement("span")
+    plus.innerHTML = "+"
+    plus.classList.add("botoes")
+
+    let adicionar = document.createElement("input")
+    adicionar.type = "number";
+    adicionar.value = "0"
+    adicionar.classList.add("input")
+
+    nome.innerHTML = imovel.produto
+
+    if (imovel.id < 6)
+        imagem.src = "https://cdn-icons-png.flaticon.com/512/2771/2771406.png"
+    else
+        imagem.src = "https://cdn-icons-png.flaticon.com/512/6602/6602190.png"
+
+    descricao.innerHTML = imovel.descricao
+    preco.innerHTML = "R$" + imovel.preco
+
+    card.appendChild(nome)
+    card.appendChild(imagem)
+    card.appendChild(descricao)
+    card.appendChild(preco)
+    card.appendChild(div)
+
+    div.appendChild(minus)
+    div.appendChild(adicionar)
+    div.appendChild(plus)
+
+    itens.appendChild(card)
 }
