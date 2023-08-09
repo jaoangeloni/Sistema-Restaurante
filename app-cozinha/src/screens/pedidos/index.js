885b9957a8d5bdffe615e3e67919e195f46a8bbf
Jao Angeloni <joaopedroangeloni@gmail.com>
2023-08-09 10:52:43 -0300

    PROJ

--- app-cozinha/src/screens/pedidos/index.js
+++ app-cozinha/src/screens/pedidos/index.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 import styles from './styles'
-
 import ItemPedido from '../../components/item';
 
 const PedidosScreen = ({ navigation }) => {
 
+    const uri = 'http://localhost:3000/pedido/cozinha';
     const [pedidos, setPedidos] = useState([]);
 
     useEffect(() => {
@@ -16,6 +16,29 @@ const PedidosScreen = ({ navigation }) => {
                 setPedidos(data);
             });
     }, []);
+
+    const concluirPedido = (id) => {
+        const corpo = {
+            id: id,
+            dataCozinha: new Date()
+        }
+
+        const options = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(corpo)
+        };
+
+        fetch(uri, options)
+            .then(resp => resp.status)
+            .then(data => {
+                if (data = 202) {
+                    navigation.navigate('HomeScreen')
+                } else {
+                    alert('Erro ao concluir pedido!')
+                }
+            });
+    }
 
     return (
         <View style={styles.container}>
